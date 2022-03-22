62e9ee534e95f75411ebc63b1e38e3226025d7c2
Samuel Ataides <sam.ataides28@gmail.com>
2022-03-22 13:53:49 -0300

    Correção de erros apontados no relatório de homologação do Banco

--- santander/CNABSantanderREM240.php
+++ santander/CNABSantanderREM240.php
@@ -139,8 +139,8 @@ class CNABSantanderREM240 extends CNABSantander {
 		$this->addField($this->getConta(), 9, '0', STR_PAD_LEFT, 'conta');
 		$this->addField($this->getVerificadorConta(), 1, ' ', STR_PAD_LEFT, 'contaDV');
 
-		$this->addField($this->getConta(), 9, '0', STR_PAD_LEFT, 'contaCobrancaDestFIDC');
-		$this->addField($this->getVerificadorConta(), 1, '0', STR_PAD_LEFT, 'verificadorCobrancaDestFIDC');
+		$this->addField('', 9, '0', STR_PAD_LEFT, 'contaCobrancaDestFIDC');
+		$this->addField('', 1, '0', STR_PAD_LEFT, 'verificadorCobrancaDestFIDC');
 
 		$this->addField("", 2, ' ', STR_PAD_LEFT);//Uso exclusivo FEBRABAN
 
@@ -155,8 +155,8 @@ class CNABSantanderREM240 extends CNABSantander {
 		$this->addField($oTitulo->getSeuNumero(), 15, '0', STR_PAD_LEFT, 'numeroDocumento');
 		$this->addField($oTitulo->getVencimento(), 8, '0', STR_PAD_LEFT, 'vencimento');
 		$this->addField($oTitulo->getValor(), 15, '0', STR_PAD_LEFT, 'valor');
-		$this->addField($this->getAgencia(), 4, '0', STR_PAD_LEFT, 'contaCobrancaBenefFIDC');
-		$this->addField($this->getVerificadorAgencia(), 1, '0', STR_PAD_LEFT, 'verificadorCobrancaBenefFIDC');
+		$this->addField('', 4, '0', STR_PAD_LEFT, 'contaCobrancaBenefFIDC');
+		$this->addField('', 1, '0', STR_PAD_LEFT, 'verificadorCobrancaBenefFIDC');
 		$this->addField("", 1, ' ', STR_PAD_LEFT);//Uso exclusivo FEBRABAN
 		$this->addField($oTitulo->getEspecieTitulo(), 2, '0', STR_PAD_LEFT, 'especieTitulo');
 		$this->addField($oTitulo->getAceite(), 1, 'N', STR_PAD_LEFT, 'aceite');
